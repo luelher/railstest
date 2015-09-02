1670adcd3a9a765f8d3fc8dec3524f9bfff28a29
Luis Hernández <luelher@gmail.com>
2015-09-02 14:39:49 -0430

    quitando acentos por error en guindows

--- app/scripts/unos.rb
+++ app/scripts/unos.rb
@@ -1,7 +1,7 @@
 
-puts "Programa para buscar los 'Unos' (1) dentro de un número en notación binaria"
+puts "Programa para buscar los 'Unos' (1) dentro de un numero en notacion binaria"
 puts
-puts "Introduzca número a Analizar:"
+puts "Introduzca numero a Analizar:"
 num = gets.chomp
 
 result = num.to_i.to_s(2).scan(/1/).count
@@ -9,6 +9,6 @@ result = num.to_i.to_s(2).scan(/1/).count
 if result == 0
   puts "El valor no es un entero"
 else
-  puts "El número #{num.to_i} contien #{result} unos en notación binaria"
+  puts "El numero #{num.to_i} contien #{result} unos en notacion binaria"
 end
 puts ""
